2743577d301e881295ba42b1e5165c5eafc99b10
Dario Zubaray <dzubaray@gmail.com>
2020-10-01 12:47:27 -0300

    Leer argumentos useParams y redireccionar Redirect

--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import { getHeroesById } from '../../selectors/getHeroeById'
 
 export const HeroeScreen = () => {
+
+    const { heroeId } = useParams()
+
+    const heroe = getHeroesById(heroeId)
+
+    if ( !heroe ) {
+        return <Redirect to="/" />
+    }
+
+    console.log( heroe )
+
     return (
         <div>
             <h1>Heroe Screen</h1>
