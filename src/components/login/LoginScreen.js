73a6a331b5b6e124abc4165b31ff0751f734ae15
Dario Zubaray <dzubaray@gmail.com>
2020-10-03 00:42:35 -0300

    login del usuario

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,22 +1,85 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { useForm } from '../../hooks/useForm'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({ history }) => {
 
-    const handleClick = () => {
+    const { dispatch } = useContext(AuthContext)
+
+    const initialState = {
+        email : '',
+        password: '',
+        logged: false
+    }
+    const [ values, handleInputChange, reset ] = useForm( initialState )
+
+    const { email, password, logged } = values
+    const handleSubmit= (e) => {
+        e.preventDefault()
+
+        console.log('handleSubmit', email, password, logged)
+
+        dispatch({
+            type: types.login,
+            payload: {
+                email,
+                password
+            }
+        })
+        reset()
         history.replace('/')
     }
 
     return (
         <div className="container mt-5">
-            <h1>Login Screen</h1>
-            <hr />
+            <div className="card bg-transparent">
+                <div className="card-title">
+                    <h3 className="text-center mt-5">Login Screen</h3>
+                </div>
 
-            <button
-                className="btn btn-primary"
-                onClick={ handleClick }
-            >
-                Login
-            </button>
+                <div className="card-body">
+                    <form onSubmit={ handleSubmit }>
+                        <div className="form-group">
+                            <label htmlFor="email">Email address:</label>
+                            <input
+                                name="email"
+                                id="email"
+                                type="email"
+                                value={ email }
+                                onChange={ handleInputChange }
+                                className="form-control"
+                                placeholder="Enter email"
+                                autoComplete="off"
+                                />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="pwd">Password:</label>
+                            <input
+                                name="password"
+                                id="password"
+                                type="password"
+                                value={ password }
+                                onChange={ handleInputChange }
+                                className="form-control"
+                                placeholder="Enter password"
+                                autoComplete="off"
+                            />
+                        </div>
+                        <div className="form-group form-check">
+                            <label className="form-check-label">
+                                <input className="form-check-input" type="checkbox" /> Remember me
+                            </label>
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
